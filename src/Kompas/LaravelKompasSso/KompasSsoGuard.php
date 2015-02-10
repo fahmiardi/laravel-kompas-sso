4935aa8643022a4b99f7c719a761d5c0fd19bfc1
Fahmi Ardi <f4hem.net@gmail.com>
2015-02-11 01:17:54 +0700

    missing KompasSsoGuard

--- src/Kompas/LaravelKompasSso/KompasSsoGuard.php
+++ src/Kompas/LaravelKompasSso/KompasSsoGuard.php
@@ -1,119 +1,32 @@
-<?php 
+<?php
+
 namespace Kompas\LaravelKompasSso;
 
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\UserProviderInterface;
-use Illuminate\Support\Facades\App;
-use Illuminate\Auth\GenericUser;
+
+use Illuminate\Auth\Guard;
 use Illuminate\Support\Facades\Session;
 
-class KompasUserProvider implements UserProviderInterface {
+class KompasSsoGuard extends Guard {
 
-    public function __construct()
+    public function user()
     {
-
-        App::make('accounts-session');
-        $this->sharedSession = null;
-        $sessionId = null;
-
-        if (isset($_GET[$this->getTokenName()])) {
-            $sessionId = $_GET[$this->getTokenName()];
-        } else if (Session::has($this->getTokenName())) {
-            $sessionId = Session::get($this->getTokenName());
-
-            if (isset($_COOKIE[$this->getTokenName()])) {
-                if ($_COOKIE[$this->getTokenName()]) {
-                    if ($sessionId != $_COOKIE[$this->getTokenName()]) {
-                        $sessionId = $_COOKIE[$this->getTokenName()];
-                    }
-                }
-            }
+        $user = parent::user();
+        if (is_null($user)) {
+            return $this->user = $this->provider->retrieveByCredentials(array());
         }
-
-        session_name(strtoupper($this->getTokenName()));
-        session_id($sessionId);
-        session_start();
-
-        $userToken = isset($_SESSION['userToken']) ? json_decode($_SESSION['userToken'], true) : false;
-        $currentUser = isset($_SESSION['currentUser']) ? json_decode($_SESSION['currentUser'], true) : false;
-
-        if ($userToken !== false && $currentUser !== false) {
-
-            Session::put($this->getTokenName(), $sessionId);
-
-            $this->sharedSession = [
-                'userToken' => $userToken,
-                'currentUser' => $currentUser
-            ];
-
-        }
-
+        return $user;
     }
 
-    public function retrieveByToken( $identifier, $token ){}
-    public function updateRememberToken( \Illuminate\Auth\UserInterface $user, $token ){}
-
-    /**
-     * Retrieve a user by their unique identifier.
-     *
-     * @param  mixed $identifier
-     * @return \Illuminate\Auth\UserInterface|null
-     */
-    public function retrieveById($identifier)
+    public function logout()
     {
-        $user = $this->retrieveByCredentials(array());
-        if ($user && $user->getAuthIdentifier() == $identifier) {
-            return $user;
-        }
+        $this->provider->destroy();
+
+        Session::forget($this->provider->getTokenName());
+        parent::logout();
     }
 
-    /**
-     * Retrieve a user by the given credentials.
-     *
-     * @param  array $credentials
-     * @return \Illuminate\Auth\UserInterface|null
-     */
-    public function retrieveByCredentials(array $credentials)
+    public function getLoginUrl()
     {
-        if ($this->sharedSession) {
-
-            $userinfo = $this->sharedSession['currentUser'];
-            $userinfo['accessToken'] = $this->sharedSession['userToken'];
-
-            return new GenericUser($userinfo);
-        }
+        return $this->provider->getLoginUrl();
     }
-
-    /**
-     * Validate a user against the given credentials.
-     *
-     * @param  \Illuminate\Auth\UserInterface $user
-     * @param  array $credentials
-     * @return bool
-     */
-    public function validateCredentials(UserInterface $user, array $credentials)
-    {
-        // this method doesn't make sense for Google auth
-        return false;
-    }
-
-    public function getLoginUrl() {
-        return "http://accounts.kompas.com/service_signin";
-    }
-
-    public function destroy()
-    {
-        session_destroy();
-    }
-
-    /**
-     * Get a unique identifier for the auth session value.
-     *
-     * @return string
-     */
-    public function getTokenName()
-    {
-        return 'kmpsid';
-    }
-
 }
